12ad3ea3a117e656e26b4b494d16d51b661f77bb
sehyun38 <162404160+sehyun38@users.noreply.github.com>
2024-06-12 05:56:01 +0000

    lesson23 끝

--- lesson-23/controllers/usersController.js
+++ lesson-23/controllers/usersController.js
@@ -31,11 +31,44 @@ module.exports = {
    * Listing 23.3 (p. 336)
    * userController.js로의 로그인과 인증 액션 추가
    */
-
+  login: (req,res)=>{
+    res.render("users/login",{
+      page: "login",
+      title: "Login Page"
+    });
+  },
   /**
    * @TODO: authenticate 액션
    */
-
+  authenticate: (req,res,next) =>{
+    User.findOne({email: req.body.email})
+    .then(user =>{
+      if(user){
+        user.passwordCompare(req.body.password)
+        .then(pwMatch =>{
+          if(pwMatch){
+            res.locals.redirect = `/users/${user._id}`;
+            res.locals.user = user;
+            res.flash("success", "login successful");
+          }
+          else{
+            res.locals.redirect = "/users/login";
+            res.flash("error", "User account not found!"); 
+          }
+        })
+        .catch()
+      }
+      else{
+        res.locals.redirect = "/users/login";
+        res.flash("error", "User account not found!")
+      }
+      next();
+    })
+    .catch(error =>{
+      console.log(`Error logging in: ${errro.message}`);
+      next(error);
+    })
+  },
   index: (req, res, next) => {
     User.find() // index 액션에서만 퀴리 실행
       .then((users) => {
@@ -115,6 +148,36 @@ module.exports = {
    * Listing 23.7 (p. 346)
    * userController.js에서 validate 액션 추가
    */
+
+  validate: (req,res,next) =>{
+    req
+      .sanitizeBody("email")
+      .normalizeEmail({
+        all_lowercase:true
+      })
+      .trim();
+
+    req 
+      .check("password", "Password cannot be empty")
+      .isEmail();
+
+    req
+    .check("password", "Password cannot be empty")
+    .notEmpty();
+
+
+
+    req.getValidationResult()
+    .then(result =>{
+      if(!result.isEmpty()){
+        let message = result.array().map(m => m.msg);
+        res.locals.redirect = "/users/new";
+        req.skip = true;
+        req.flash("error",message.join(" and "));
+      }
+      next();
+    })
+  },
 
   /**
    * [노트] 폼 데이터를 다시 채우기 위해 다양한 방법을 선택할 수 있다. (연구해보면)
